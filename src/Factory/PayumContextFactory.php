b946a6245babb3045b9a10c3cddfc41aec23b6cd
iampersistent <corvus@zestic.com>
2025-01-09 08:38:35 -0600

    strip down factory

--- src/Factory/PayumContextFactory.php
+++ src/Factory/PayumContextFactory.php
@@ -3,39 +3,14 @@ declare(strict_types=1);
 
 namespace OLPS\SimpleShopComponent\Factory;
 
-use ConfigValue\GatherConfigValues;
 use OLPS\SimpleShop\Entity\PayumContext;
 use Payum\Core\Payum;
-use Payum\Core\PayumBuilder;
 use Psr\Container\ContainerInterface;
 
 class PayumContextFactory
 {
-    // todo: set up repo
     public function __invoke(ContainerInterface $container): PayumContext
     {
-        $payum = $this->payumFactory($container);
-
-        return new PayumContext($payum);
-    }
-
-    private function payumFactory(ContainerInterface $container): Payum
-    {
-        $config = (new GatherConfigValues)($container, 'payum');
-        $builder = new PayumBuilder();
-
-        foreach ($config['gateways'] as $name => $gateway) {
-            $builder->addGateway($name, $gateway);
-        }
-
-        $builder->setTokenStorage($this->tokenStorageFactory($config['tokenStorage']));
-
-        // $builder->addStorage($modelClass, $storage);
-        return $builder->getPayum();
-    }
-
-    private function tokenStorageFactory(array $config): StorageInterface
-    {
-        
+        return new PayumContext($container->get(Payum::class));
     }
 }
